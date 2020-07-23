9f8c9ac8a39d773ed3e86721a67ab27d2937b04c
Callum France <france.callum@gmail.com>
2020-07-23 16:36:52 +0800

    Adding signage into roller_factory

--- dice-roller/roller_factory.py
+++ dice-roller/roller_factory.py
@@ -9,11 +9,51 @@ class RollerFactory():
 
 
     @staticmethod
-    def create_roller_by_string(in_roll_str: str="2d8 3d6 7") -> List[Roller]:
-        new_rolls = list()
+    def create_roller_by_string(in_roll_str: str="2d8 - 3d6 + 7 + 2d4 - 6") -> List[Roller]:
+        """
+        **Not fully implemented**
 
-        for roll_clips in in_roll_str.split():
-            roll_di = roll_clips.split("d")
+        1. Remove all whitespace
+        2. Split along '-'
+        3. For each '-' string, split along '+'
+        4. Add a '-' sign to the first element of each '-' list except all[0]
+        5. Add a '+' sign to all other elements of each '-' list
+        6. Create dice using 'd' or modifier if not using 'd'
+        """
+        new_rolls = list()
+        raw_rolls = list()
+        sign_sequence = list() # list of either '-' or '+' corresponding to roll
+
+        in_roll_str = "".join(in_roll_str.split()) # Removes all whitespace
+        minus_split = in_roll_str.split("-")
+        sign_sequence = ['+'] + ['-'] * (len(minus_split) - 1)
+
+        print("Pre-Adding:", minus_split, sign_sequence)
+
+        sign_sentinel = 0
+        for _ in minus_split:
+            print("Next in minus_split: ", _)
+            for n, plus_split in enumerate(_.split("+")):
+                # TODO Requires fixing array values for a proper insertion...
+                raw_rolls.append(plus_split)
+                temp_sentinel = sign_sentinel + 1
+                print("\tplus_split: ", plus_split, "\t\t", sign_sentinel, temp_sentinel)
+
+                print("\t\t", sign_sequence[:sign_sentinel], "[+]", sign_sequence[sign_sentinel + 1:],)
+
+                if n != 0:
+                    sign_sequence = sign_sequence[:sign_sentinel] + \
+                            ["+"] + \
+                            sign_sequence[sign_sentinel + 1:]
+
+                print("\t\t", sign_sequence)
+
+                sign_sentinel = temp_sentinel + 1
+
+        print(raw_rolls, sign_sequence)
+
+        for _ in raw_rolls:
+            roll_di = _.split("d")
 
             if len(roll_di) < 2:
                 new_rolls.append(Modifier(int(roll_di[0])))
