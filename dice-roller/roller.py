e17ab9d7685811f2ecc2fc260a63e7232292fc16
Callum France <france.callum@gmail.com>
2020-07-23 15:38:15 +0800

    Added a Readme, moved py files into dice-roller folder

---
+++ dice-roller/roller.py
@@ -0,0 +1,55 @@
+from abc import ABCMeta, abstractmethod
+from typing import Dict
+
+
+class Roller(metaclass=ABCMeta):
+    """
+    The Roller class is an interface of things of indeterminate value.
+
+    :param str style: The hexadecimal string colour of the Roller object
+    :param str sign: The + or - sign indicator of the Roller's value
+    """
+
+
+    @property
+    def style(self):
+        return self.style
+
+    @style.setter
+    def style(self, style: str=None):
+        self._style = style
+
+    @property
+    def sign(self):
+        return self.sign
+
+    @sign.setter
+    def sign(self, sign: str="+"):
+        self._sign = sign
+
+    @abstractmethod
+    def roll(self) -> int:
+        """
+        The act of rolling the Roller to produce a value.
+
+        :return: The rolled value
+        :rtype: int
+
+        :example:
+
+        >>> a = Dicer(6)
+        >>> a.roll()
+        4
+        """
+        pass
+
+    @abstractmethod
+    def probability_dist(self) -> Dict[int, float]:
+        """
+        Gives the likelihoods of each number being rolled by the Roller
+
+        :return: A dictionary of probability distributions, where each key
+            is an integer that contains its probability of getting.
+        :rtype: dict
+        """
+        pass
---
+++ dice-roller/roller_packet.py
@@ -0,0 +1,91 @@
+from typing import Dict, Tuple, List
+
+from roller import Roller
+from roller_factory import RollerFactory
+from dicer import Dicer
+from modifier import Modifier
+
+
+class RollerPacket():
+
+
+    def __init__(self):
+        self.rollers = list()
+
+    def roll_all(self) -> Tuple[int, List[Tuple[int, Roller]]]:
+        """
+        :return: A tuple that contains:
+            - The integer total that has been rolled
+            - A list of every Roller's total that was rolled, plus that Roller
+        :rtype: Tuple
+        """
+        total = 0
+        rolled = list()
+
+        for i in self.rollers:
+            rolled.append((i.roll(), i))
+            total += rolled[-1][0]
+
+        return (total, rolled)
+
+    def statistics(self) -> Dict[int, float]:
+        """
+        Determines the probabililty of a number being rolled from RollerPacket
+
+        All of the probabilities, when summed, should equal 1.0
+
+        :return: A dictionary where the key is the number that was rolled, and
+            the value is the decimal probablility that is was rolled (out of 1).
+        :rtype: Dict
+        """
+        all_probs = list()
+        total_prob = dict()
+
+        for i in self.rollers:
+            all_probs.append(i.probability_dist())
+
+        total_prob = all_probs.pop(0)
+
+        while len(all_probs) > 0:
+            last_probs = total_prob
+            next_probs = all_probs.pop(0)
+            total_prob = dict()
+
+            for lp_key in last_probs:
+                for np_key in next_probs:
+                    semi_total = total_prob.get(lp_key + np_key, 0)
+                    total_prob[lp_key + np_key] = semi_total + \
+                            (last_probs[lp_key] * next_probs[np_key])
+
+        return total_prob
+
+
+if __name__ == '__main__':
+    rp = RollerPacket()
+    rf = RollerFactory()
+
+    rp.rollers = rf.create_roller_by_string("3d4 2d6 5")
+    print(rp.rollers)
+
+    run = rp.roll_all()
+
+    print(run[0])
+
+    for x in run[1]:
+        print(x[0], end="\t")
+        if isinstance(x[1], Dicer):
+            print("d", x[1].sides, end="")
+        if isinstance(x[1], Modifier):
+            print("M", x[1].value, end="")
+        print()
+
+
+    stats = rp.statistics()
+
+    stat_1 = 0
+
+    for key in stats:
+        stat_1 += stats[key]
+
+    print("stat_1 is ", stat_1)
+    print(stats)
