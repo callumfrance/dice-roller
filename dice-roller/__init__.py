5713c86d3c51c6804b69abd13097ad90ae525366
callumfrance <france.callum@gmail.com>
2020-08-02 23:56:52 +0800

    custom dice functionality added; nicer build append text formatting

--- dice-roller/__init__.py
+++ dice-roller/__init__.py
@@ -34,12 +34,21 @@ def create_app(test_config=None):
             mystr_counter = 0
             for n, key in enumerate(request.form):
                 if request.form[key]:
+
                     if request.form[key] == '':
                         continue
+                    elif key == 'dXX':
+                        pass
                     elif (request.form[key][0] != '-') and (mystr_counter != 0):
                         mystr += ' + '
+
                     mystr_counter += 1
+
                     if key == "m":
+                        mystr += request.form[key]
+                    elif key == 'dX':
+                        mystr += request.form[key] + 'd'
+                    elif key == 'dXX':
                         mystr += request.form[key]
                     else:
                         mystr += request.form[key] + key
